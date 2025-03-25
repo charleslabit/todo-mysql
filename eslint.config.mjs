69fd33639ff68549f44adeccceed9ac797e4421b
charles.labit <charleslabit@gmail.com>
2025-03-25 15:52:08 +0800

    configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Example: Disabling the rule react/no-unescaped-entities
+      "react/no-unescaped-entities": "off",
+
+      // Add any other rules you'd like to disable or customize
+      "no-console": "warn", // Example: Warnings for console logs
+      "react/prop-types": "off", // Example: Disabling prop-types validation
+    },
+  },
 ];
 
 export default eslintConfig;
